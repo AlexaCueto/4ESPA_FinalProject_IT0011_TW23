40e0a177915997f1b473df81df1c50eb4d1d3d6b
Alexa Cueto <167626858+AlexaCueto@users.noreply.github.com>
2025-03-25 15:48:54 +0800

    edited comments

--- src/main.py
+++ src/main.py
@@ -12,8 +12,8 @@ def main_menu():
     tk.Label(root, text="Welcome to the System", font=("Roboto", 18, 'bold'), bg="#e6e6fa", fg="#9370DB").pack(pady=20)
 
     tk.Button(root, text="Sign Up", font=("Roboto", 14), background='#9370DB', fg="white", command=lambda: signUpWindow(root)).pack(pady=10)
-    #tk.Button(root, text="Search Records", font=("Roboto", 14), background='#9370DB', fg="white", command=open_search_window(root)).pack(pady=10)
-    #tk.Button(root, text="View Records", font=("Roboto", 14), background='#9370DB', fg="white", command=open_search_window(root)).pack(pady=10)
+    #tk.Button(root, text="Search Records", font=("Roboto", 14), background='#9370DB', fg="white", command=searchRecordWindow(root)).pack(pady=10)
+    #tk.Button(root, text="View Records", font=("Roboto", 14), background='#9370DB', fg="white", command=viewRecordWindow(root)).pack(pady=10)
     tk.Button(root, text="Exit", font=("Roboto", 14), background='#9370DB', fg="white", command=root.quit).pack(pady=20)
 
     root.mainloop()
